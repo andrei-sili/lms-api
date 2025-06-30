ae7b5390cc953794044159b7a60d1c415b807ab4
Andrei <andrei.sili1985@gmail.com>
2025-06-30 13:09:12 +0200

    Initial commit: Django LMS project structure with settings split and Makefile

---
+++ Makefile
@@ -0,0 +1,24 @@
+run:
+	python manage.py runserver
+
+migrate:
+	python manage.py migrate
+
+makemigrations:
+	python manage.py makemigrations
+
+shell:
+	python manage.py shell
+
+superuser:
+	python manage.py createsuperuser
+
+test:
+	pytest
+
+freeze:
+	pip freeze > requirements.txt
+
+startapp:
+	@read -p "App name: " app; \
+	python manage.py startapp $$app apps/$$app
---
+++ lms_api/settings/production.py
@@ -0,0 +1,22 @@
+# lms_api/settings/production.py
+
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    }
+}
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
