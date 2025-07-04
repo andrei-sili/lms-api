2ffcff73067ea44dadfb9445335b89c52ce635fa
Andrei <andrei.sili1985@gmail.com>
2025-07-04 11:51:22 +0200

    Set up UserViewSet routing with JWT auth endpoints in users app URLs

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,21 @@
 # apps/users/urls.py
 
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
-urlpatterns = [
+from apps.users.views import UserViewSet
+
+router = DefaultRouter()
+router.register('', UserViewSet, basename='user')
+
+urlpatterns = router.urls
+
+urlpatterns += [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
