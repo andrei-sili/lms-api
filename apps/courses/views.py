0e3971c11c4c2d521ec898a68a7083a450056ee7
Andrei <andrei.sili1985@gmail.com>
2025-07-07 15:48:18 +0200

    Implement CourseViewSet, CategoryViewSet, and CourseCategoryViewSet with proper permissions

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+#  apps/courses/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Course, Category, CourseCategory
+from .serializers import CourseSerializer, CategorySerializer, CourseCategorySerializer
+from .permissions import IsOwnerTeacherOrReadOnly, IsAdminOrReadOnly
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated & IsOwnerTeacherOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(teacher=self.request.user)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class CourseCategoryViewSet(viewsets.ModelViewSet):
+    queryset = CourseCategory.objects.all()
+    serializer_class = CourseCategorySerializer
+    permission_classes = [IsAuthenticated & IsOwnerTeacherOrReadOnly]
+
+    def perform_create(self, serializer):
+        course = serializer.validated_data['course']
+        if self.request.user != course.teacher or self.request.user.role != 'teacher':
+            raise PermissionDenied("You don't have permission to associate categories to this course.")
+        serializer.save()
