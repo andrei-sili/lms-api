c07916791ff5daca74d08cb34ec0ab7105189d2f
Andrei <andrei.sili1985@gmail.com>
2025-07-08 13:43:27 +0200

    Customize Course and Category admin display

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,7 @@
+# apps/courses/admin.py
 from django.contrib import admin
+from .models import Course, Category, CourseCategory
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Category)
+admin.site.register(CourseCategory)
