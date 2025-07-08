54f2002009b9128d87040f68ffed3578901d8e90
Andrei <andrei.sili1985@gmail.com>
2025-07-08 12:16:05 +0200

    Remove JWT endpoints from users/urls.py (moved to root urls)

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,6 @@
 # apps/users/urls.py
 
-from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 from apps.users.views import UserViewSet
 
@@ -14,9 +8,3 @@ router = DefaultRouter()
 router.register('', UserViewSet, basename='user')
 
 urlpatterns = router.urls
-
-urlpatterns += [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
