ae4aaa82e1d792d1d246801069f5ed152c6eb58d
Andrei <andrei.sili1985@gmail.com>
2025-07-09 15:24:31 +0200

    Update LessonViewSet to use custom serializer logic and improved permission checks.

--- apps/lessons/views.py
+++ apps/lessons/views.py
@@ -3,14 +3,19 @@
 from rest_framework import viewsets
 from apps.courses.permissions import IsOwnerTeacherOrReadOnly
 from apps.lessons.models import Lesson
-from apps.lessons.serializers import LessonSerializer
+from apps.lessons.serializers import LessonReadSerializer, LessonWriteSerializer
 
 
 class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
     permission_classes = [IsOwnerTeacherOrReadOnly]
 
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return LessonReadSerializer
+        return LessonWriteSerializer
+
     def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
+        serializer.save(teacher=self.request.user)
+
 
