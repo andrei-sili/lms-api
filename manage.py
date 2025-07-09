5e8213e20763c14ab34d7e3a7908652af304c04e
Andrei <andrei.sili1985@gmail.com>
2025-07-09 13:13:14 +0200

    Enhance manage.py to auto-detect environment via .env (production/development)

--- manage.py
+++ manage.py
@@ -2,11 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
+
+env_mode = env("ENV", default="development")
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms_api.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"lms_api.settings.{env_mode}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +25,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
