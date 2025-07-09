8f6d92aa75232b01c5c4a7895abeaec539fca684
Andrei <andrei.sili1985@gmail.com>
2025-07-09 11:33:37 +0200

    Created serializers for Lesson model

---
+++ apps/lessons/serializers.py
@@ -0,0 +1,26 @@
+# apps/lessons/serializers.py
+
+from rest_framework import serializers
+
+from apps.courses.serializers import CourseSerializer
+from apps.lessons.models import Lesson
+from apps.quizzes.serializers import QuizSerializer
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    course = CourseSerializer(read_only=True)
+    quizzes = QuizSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Lesson
+        fields = [
+            'title',
+            'content',
+            'video_url',
+            'order',
+            'course',
+            'quizzes',
+            'created_at'
+        ]
+
+        read_only_fields = ['id', 'course', 'created_at']
