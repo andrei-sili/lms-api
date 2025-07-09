fa7e383f8994dd8d44bd1b406e04db47ca7224e6
Andrei <andrei.sili1985@gmail.com>
2025-07-09 13:13:13 +0200

    Add Lesson viewset with queryset filtering and permission integration

--- apps/lessons/views.py
+++ apps/lessons/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# apps/lessons/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from apps.courses.permissions import IsOwnerTeacherOrReadOnly
+from apps.lessons.models import Lesson
+from apps.lessons.serializers import LessonSerializer
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+    permission_classes = [IsOwnerTeacherOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
