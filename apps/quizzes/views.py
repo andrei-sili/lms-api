72499b494430b650d30ff54a7d059ff78e711dc8
Andrei <andrei.sili1985@gmail.com>
2025-07-09 13:13:13 +0200

    Add Quiz viewset with custom permissions and optimized queryset

--- apps/quizzes/views.py
+++ apps/quizzes/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# apps/quizzes/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from apps.courses.permissions import IsOwnerTeacherOrReadOnly
+from apps.quizzes.models import Quiz, Question, Answer
+from apps.quizzes.serializers import QuizSerializer, QuestionSerializer, AnswerSerializer
+
+
+class QuizViewSet(viewsets.ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    permission_classes = [IsOwnerTeacherOrReadOnly]
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [IsOwnerTeacherOrReadOnly]
+
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsOwnerTeacherOrReadOnly]
