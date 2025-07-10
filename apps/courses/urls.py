ef4a1746a4d6183efe36da1958db246165ceeb76
Andrei <andrei.sili1985@gmail.com>
2025-07-10 10:01:19 +0200

    Refactor and optimize API routing in courses app

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from apps.courses.views import CourseViewSet, CourseCategoryViewSet, CategoryViewSet
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='course')
-router.register(r'category', CategoryViewSet, basename='category')
-router.register(r'course_categories', CourseCategoryViewSet, basename='course_category')
+router.register(r'', CourseViewSet, basename='courses')
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'course_categories', CourseCategoryViewSet, basename='course_categories')
 
 urlpatterns = router.urls
