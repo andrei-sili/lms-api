22426cb6ff07651bef840e94c96ce175ee2c0683
Andrei <andrei.sili1985@gmail.com>
2025-07-10 12:11:43 +0200

    Added UserQuizAttempt model to store user quiz attempts and scores

--- apps/quizzes/models.py
+++ apps/quizzes/models.py
@@ -1,7 +1,8 @@
 # apps/quizzes/models.py
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from apps.lessons.models import Lesson
+from apps.users.models import User
 
 
 class Quiz(models.Model):
@@ -28,3 +29,13 @@ class Answer(models.Model):
 
     def __str__(self):
         return f"Answer: {self.text[:50]}..."
+
+
+class UserQuizAttempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_quiz_attempts')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='attempted_quizzes')
+    score = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} → {self.quiz} ({self.score})"
