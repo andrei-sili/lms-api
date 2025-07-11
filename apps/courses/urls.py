35d6aea9b76cbed907f16ba5fe560f4e66c684c9
Andrei <andrei.sili1985@gmail.com>
2025-07-11 13:54:31 +0200

    feat(courses):Added routes for enrollment endpoints

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,11 +1,11 @@
 #  apps/courses/urls.py
 
 from rest_framework.routers import DefaultRouter
-from apps.courses.views import CourseViewSet, CourseCategoryViewSet, CategoryViewSet
+from apps.courses.views import CourseViewSet, CourseCategoryViewSet, CategoryViewSet, EnrollmentViewSet
 
 router = DefaultRouter()
 router.register(r'', CourseViewSet, basename='courses')
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'course_categories', CourseCategoryViewSet, basename='course_categories')
-
+router.register(r'enrollments', EnrollmentViewSet, basename='enrollment')
 urlpatterns = router.urls
