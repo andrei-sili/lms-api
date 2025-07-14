4a565207b74ec92787975f1292128b16b66d4651
Andrei <andrei.sili1985@gmail.com>
2025-07-14 12:29:40 +0200

    feat(quizzes): implement UserQuizAttemptViewSet filtered by authenticated user

--- apps/quizzes/views.py
+++ apps/quizzes/views.py
@@ -1,9 +1,12 @@
 # apps/quizzes/views.py
 
 from rest_framework import viewsets
-from apps.courses.permissions import IsOwnerTeacherOrReadOnly
-from apps.quizzes.models import Quiz, Question, Answer
-from apps.quizzes.serializers import QuizReadSerializer, QuestionSerializer, AnswerSerializer, QuizWriteSerializer
+from rest_framework.permissions import IsAuthenticated
+
+from apps.courses.permissions import IsOwnerTeacherOrReadOnly, HasActiveSubscription
+from apps.quizzes.models import Quiz, Question, Answer, UserQuizAttempt
+from apps.quizzes.serializers import QuizReadSerializer, QuestionSerializer, AnswerSerializer, QuizWriteSerializer, \
+    UserQuizAttemptSerializer
 
 
 class QuizViewSet(viewsets.ModelViewSet):
@@ -29,3 +32,17 @@ class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
     permission_classes = [IsOwnerTeacherOrReadOnly]
+
+
+class UserQuizAttemptViewSet(viewsets.ModelViewSet):
+    serializer_class = UserQuizAttemptSerializer
+    permission_classes = [IsAuthenticated, HasActiveSubscription]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return UserQuizAttempt.objects.all()
+        return UserQuizAttempt.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
