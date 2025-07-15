b5aa62ab4e1ebb164660ac4b58903e902182d420
Andrei <andrei.sili1985@gmail.com>
2025-07-15 12:43:06 +0200

    feat(urls): register route for lesson progress endpoint

--- apps/lessons/urls.py
+++ apps/lessons/urls.py
@@ -1,11 +1,13 @@
 #  apps/lessons/urls.py
 
 from rest_framework.routers import DefaultRouter
-from apps.lessons.views import LessonViewSet, AttachmentViewSet, CommentViewSet
+from apps.lessons.views import LessonViewSet, AttachmentViewSet, CommentViewSet, LessonProgressViewSet
 
 router = DefaultRouter()
 router.register(r'', LessonViewSet, basename='lessons')
 router.register(r'attachments', AttachmentViewSet, basename='attachment')
 router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'progress', LessonProgressViewSet, basename='lesson_progress')
+
 
 urlpatterns = router.urls
