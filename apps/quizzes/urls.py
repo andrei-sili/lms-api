5294fda44c0309b2034fafe5e0d2001501d57341
Andrei <andrei.sili1985@gmail.com>
2025-07-22 13:23:41 +0200

    feat(urls): register UserQuizAttemptViewSet under 'attempts' endpoint

--- apps/quizzes/urls.py
+++ apps/quizzes/urls.py
@@ -2,11 +2,12 @@
 
 from rest_framework.routers import DefaultRouter
 
-from apps.quizzes.views import QuizViewSet, QuestionViewSet, AnswerViewSet
+from apps.quizzes.views import QuizViewSet, QuestionViewSet, AnswerViewSet, UserQuizAttemptViewSet
 
 router = DefaultRouter()
 router.register(r'', QuizViewSet, basename='quizzes')
 router.register(r'questions', QuestionViewSet, basename='questions')
 router.register(r'answers', AnswerViewSet, basename='answers')
+router.register(r'attempts', UserQuizAttemptViewSet, basename='attempts')
 
 urlpatterns = router.urls
